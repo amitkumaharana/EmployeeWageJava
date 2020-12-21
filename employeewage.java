e8edb6d2540e4d26c389961171a3a3c32a30dfb5
amitkumaharana <ak657maharana@gmail.com>
2020-12-21 22:04:07 +0530

    Calculating wages untill either total working hours of 100 is reached or days of20 is reached

--- employeewage.java
+++ employeewage.java
@@ -1,9 +1,8 @@
 public class employeewage{
 	public static void main(String[] args){
 		System.out.println("Welcome to Employee Wage Computation Program");
-		int daily_employee_wage = 0, wage_per_hour = 20, full_day_hour = 8, part_time_hour = 8, monthly_wage = 0;
-		int i;
-		for(i=1;i<=20;i++){
+		int daily_employee_wage = 0, wage_per_hour = 20, full_day_hour = 8, part_time_hour = 8, monthly_wage = 0, total_working_hour = 0, total_working_days = 0;
+		while(total_working_hour<100 && total_working_days<20){
 			double employee_attendance_check=Math.floor(Math.random()*10)%2;
 			switch ((int)employee_attendance_check){
 				case 0:
@@ -14,10 +13,13 @@ public class employeewage{
 							daily_employee_wage = wage_per_hour * part_time_hour;
 							System.out.println("Worked Part Time");
 							System.out.println("Part time daily working wage is  "+daily_employee_wage);
+							total_working_hour += part_time_hour;
 							break;
 						case 1:
-							daily_employee_wage = wage_per_hour * full_day_hour;						System.out.println("Worked Full Time");
+							daily_employee_wage = wage_per_hour * full_day_hour;
+							System.out.println("Worked Full Time");
 							System.out.println("Full time daily working wage is  "+daily_employee_wage);
+							total_working_hour += full_day_hour;
 							break;
 					}
 					break;
@@ -27,8 +29,11 @@ public class employeewage{
 					daily_employee_wage = 0;
 					break;
 			}
+			total_working_days++;
 			monthly_wage += daily_employee_wage;
 		}
 		System.out.println("Monthly wage of employee is  "+monthly_wage);
+		System.out.println("Total working days are "+total_working_days);
+		System.out.println("Total working hours are "+total_working_hour);
 	}
 }

4ace727c3266b2bc215db9098958df2cf62f2d2f
amitkumaharana <ak657maharana@gmail.com>
2020-12-21 16:27:36 +0530

    Calculating Daily Employee Wage

--- employeewage.java
+++ employeewage.java
@@ -1,15 +1,15 @@
 public class employeewage{
 	public static void main(String[] args){
 		System.out.println("Welcome to Employee Wage Computation Program");
-		int i;
-		for(i=0;i<=20;i++){
-			double employee_attendance_check=Math.floor(Math.random()*10)%2;
-			if (employee_attendance_check==0){
-				System.out.println("Employee Present");
-			}
-			else{
-				System.out.println("Employee Absent");
-			}
+		int daily_employee_wage, wage_per_hour = 20, full_day_hour = 8;
+		double employee_attendance_check=Math.floor(Math.random()*10)%2;
+		if (employee_attendance_check==0){
+			System.out.println("Employee Present");
+			daily_employee_wage = wage_per_hour * full_day_hour;
+			System.out.println(daily_employee_wage);
+		}
+		else{
+			System.out.println("Employee Absent");
 		}
 	}
 }

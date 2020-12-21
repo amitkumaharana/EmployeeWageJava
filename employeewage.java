1c26b2010264de1c7623648418a26784ce6fb54d
amitkumaharana <ak657maharana@gmail.com>
2020-12-21 16:17:09 +0530

    Attendance Checking

--- employeewage.java
+++ employeewage.java
@@ -1,15 +1,12 @@
 public class employeewage{
 	public static void main(String[] args){
 		System.out.println("Welcome to Employee Wage Computation Program");
-		int i;
-		for(i=0;i<=20;i++){
-			double employee_attendance_check=Math.floor(Math.random()*10)%2;
-			if (employee_attendance_check==0){
-				System.out.println("Employee Present");
-			}
-			else{
-				System.out.println("Employee Absent");
-			}
+		double employee_attendance_check=Math.floor(Math.random()*10)%2;
+		if (employee_attendance_check==0){
+			System.out.println("Employee Present");
+		}
+		else{
+			System.out.println("Employee Absent");
 		}
 	}
 }
